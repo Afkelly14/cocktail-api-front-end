a4e6f12f5ae5f34c781c9de21bedd5d10f217da7
Alex_Kelly <afkelly14@Gmail.com>
2020-07-31 11:58:43 -0400

    CRUD methods added

--- cocktail-api-front-end/src/App.js
+++ cocktail-api-front-end/src/App.js
@@ -1,12 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
 
 //link the API from heroku
-let url = 'https://find-a-cocktail.herokuapp.com/drinks'
+let url = "https://find-a-cocktail.herokuapp.com/drinks";
 
+//CRUD paths
 
+const optionGET = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
+};
 
+const optionPOST = {
+  method: "POST",
+  headers: {
+    Accept: "application/json",
+  },
+};
 
+const optionPUT = {
+  method: "PUT",
+  headers: {
+    Accept: "application/json",
+  },
+};
+
+const optionDELETE = {
+  method: "DELETE",
+  headers: {
+    Accept: "application/json",
+  },
+};
 
 export default App;
